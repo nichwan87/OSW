115e4f0cf59c289b971b189582c746799cdabae8
Nikku <nichwan_is@hotmail.com>
2021-04-16 18:43:21 +1000

    current progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,11 +29,12 @@ document.getElementById('now').innerHTML = today + ' @ ' + t_str;
  setTimeout(timer,1000);
 }
 
-//weatherSection
+//currentWeatherSection
 const apiKey = '110b8cbff5eb9ed03ea0acc26ca5ed4e';
 const latitude = -37.810971;
 const longitude = 144.938905;
 const unit = 'metric';
+
 
 var requestURL = "https://api.openweathermap.org/data/2.5/onecall";
 requestURL += "?lat=" + latitude;
@@ -46,10 +47,37 @@ fetch(requestURL)
 .then(data => //console.log(data))
 {
 var dailyArray = data.daily;
-var thirdDay = dailyArray[2];
-var thirdDayTemp = thirdDay.temp;
-var thirdDayMax = thirdDayTemp.max;
-document.getElementById('imaTenki').innerHTML = thirdDayMax + '°c'
+var onDay = dailyArray[0];
+var onDayTemp = onDay.temp;
+var onDayMax = onDayTemp.max;
+document.getElementById('imaTenki').innerHTML = onDayMax + '°c'
+});
+
+//weatherMap
+
+var requestURL = 'https://tile.openweathermap.org/map/temperature/5/135.0879/-25.2844.png&appid=213a2c19a73b9315ea18682adda38f43';
+
+
+
+fetch(requestURL)
+.then(response => response.json())
+.then(data => console.log(data))
+document.getElementById('weather').innerHTML = requestURL;
+
+var requestURL2 = "https://history.openweathermap.org/data/2.5/aggregated/year"
+requestURL2 += "?lat=" + latitude;
+requestURL2 += "&lon=" + longitude;
+requestURL2 += "&appid=" + apiKey;
+requestURL2 += "&units=" + unit;
+
+fetch(requestURL2)
+.then(response => response.json())
+.then(data => //console.log(data))
+{
+var yearData = data.result;
+var results = yearData[0];
+
+document.getElementById('stats').innerHTML = results
 });
 
 //displaying and hiding sections
@@ -63,12 +91,34 @@ const history = document.getElementById('history');
 const pcSearch = document.getElementById('pcSearch');
 const hide = document.getElementById('exhist');
 
-goButton.addEventListener('click' , ()=> {
+
+
+goButton.addEventListener('click', e => {
+    e.preventDefault();
     stats.style.opacity = '30%';
     weather.style.opacity = '30%';
     results.style.display = 'block';
     results.style.zIndex = '100';
+    const inputVal = document.querySelector('#inputVal');
+    inputVal.addEventListener('keypress');
+
+    const apiKey2 = '213a2c19a73b9315ea18682adda38f43';
+    var requestURL3 = 'https://api.openweathermap.org/data/2.5/weather';
+    requestURL3 += "?q=" + inputVal
+    requestURL3 += "&appid=" + apiKey2;
+    requestURL3 += "&units=" + unit;
+
+    fetch(requestURL3)
+  
+  
+    window.localStorage['input'] = document.getElementById('inputValue').value;
+    document.getElementById('pcSearch').value = window.localStorage['input'];
 });
+
+
+
+
+
 
 exit.addEventListener('click' , ()=> {
     stats.style.opacity = '93%';
